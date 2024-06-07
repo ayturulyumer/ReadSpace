aee37f49d92691380be15ec94fc65d3f5481f7ad
ajummer <makeudreamempty@gmail.com>
2024-06-07 20:36:42 +0300

    Add filter menu

--- client/readspace/src/app/catalog/page.jsx
+++ client/readspace/src/app/catalog/page.jsx
@@ -1,6 +1,10 @@
+import BookFilters from "../components/BookFilters/BookFilters.jsx";
 
+BookFilters
 export default function Catalog() {
   return (
-    
-  )
+    <div className="w-fit h-screen">  
+    <BookFilters/>
+    </div>
+  );
 }
