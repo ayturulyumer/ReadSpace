31196283887a254264d0835e2793be027abec226
ajummer <makeudreamempty@gmail.com>
2024-06-09 15:34:56 +0300

    Add conditional book actions

--- client/readspace/src/app/components/BookCard/BookCard.jsx
+++ client/readspace/src/app/components/BookCard/BookCard.jsx
@@ -2,14 +2,15 @@ import { BsCartPlus } from "react-icons/bs";
 import { IoIosInformationCircleOutline } from "react-icons/io";
 import { CiHeart } from "react-icons/ci";
 import Rating from "../Rating/Rating.jsx";
+import BookActionsOverlay from "../BookActionsOverlay/BookActionsOverlay.jsx";
 
-export default function BookCard({ data, isBestSeller, size }) {
-  const cardSizeClasses = size === "large" ? "w-72 h-full" : "w-52 h-full";
+export default function ({ data, isBestSeller, size, actionsOverlay }) {
+  const cardSizeClasses = size === "large" ? "w-64 h-full" : "w-52 h-full";
 
   return (
     <div className="indicator">
       {isBestSeller && (
-        <span className="indicator-item indicator-center badge font-medium badge-accent text-white md:indicator-end">
+        <span className="indicator-item indicator-center badge font-medium badge-secondary text-primary-content md:indicator-end">
           Best Seller
         </span>
       )}
@@ -29,19 +30,18 @@ export default function BookCard({ data, isBestSeller, size }) {
           <p>{data.author}</p>
           <Rating />
           <p className="text-red-500">${data.price}</p>
-        </div>
-        <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-          <div className="card-actions space-x-2 flex flex-col text-center items-center">
-            <button className="btn w-full btn-outline btn-accent text-white">
-              Add to cart <BsCartPlus style={{ fontSize: "2em" }} />
-            </button>
-            <button className="btn w-full btn-outline self-center btn-primary text-white">
-              Add to wishlist <CiHeart style={{ fontSize: "2em" }} />
-            </button>
-            <button className="btn w-full btn-outline btn-secondary">
-              Details <IoIosInformationCircleOutline style={{ fontSize: "2em" }} />
-            </button>
-          </div>
+          {actionsOverlay ? (
+            <BookActionsOverlay />
+          ) : (
+            <div className="flex self-end gap-2">
+              <button>
+                <BsCartPlus style={{ fontSize: "2em" }} />
+              </button>
+              <button>
+                <CiHeart style={{ fontSize: "2em" }} />
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- client/readspace/src/app/components/BooksCatalog/BooksCatalog.jsx
+++ client/readspace/src/app/components/BooksCatalog/BooksCatalog.jsx
@@ -1,106 +1,15 @@
-import React from 'react'
-
+import { bestSellers } from "../../../../utils/dummyData.js";
+import BookCard from "../BookCard/BookCard.jsx";
 export default function BooksCatalog() {
   return (
     <div className="relative flex h-fit w-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-    <div className="max-w-screen-md mx-auto">
-      <div className="grid grid-cols-4 gap-6">
-        <div className="group pb-24 relative overflow-hidden">
-          <div className="group-hover:translate-y-0 transition-all duration-700 translate-y-full top-0 right-0 bottom-24 left-0 absolute bg-gradient-to-b from-transparent to-orange-600 z-10" />
-          <img
-            src="https://picsum.photos/300/300"
-            className="transition-all group-hover:scale-125 duration-700 mr-4 h-60 w-auto"
-            alt=""
-          />
-          <div className="bg-orange-600 absolute z-10 bottom-0 left-0 w-full h-24 flex flex-col justify-center items-center">
-            <div className="z-20 absolute -top-5 w-full flex justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="group-hover:bg-white group-hover:text-orange-600 group-hover:rotate-180 w-10 h-10 bg-black text-white p-2 rounded-full transition-all"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
-                />
-              </svg>
-            </div>
-            <div className="group-hover:hidden transition-all duration-1000 w-4 absolute overflow-hidden inline-block right-0 -top-6">
-              <div className="h-6  bg-orange-900 -rotate-45 transform origin-bottom-right" />
-            </div>
-            <h2 className="font-bold">Bob haris</h2>
-            <span className="text-white">Sales executive</span>
-          </div>
-        </div>
-        <div className="group pb-24 relative overflow-hidden">
-          <div className="group-hover:translate-y-0 transition-all duration-700 translate-y-full top-0 right-0 bottom-24 left-0 absolute bg-gradient-to-b from-transparent to-orange-600 z-10" />
-          <img
-            src="https://picsum.photos/300/300"
-            className="absolute transition-all group-hover:scale-125 duration-700 mr-4 h-60 right-0"
-            alt=""
-          />
-          <div className="bg-orange-600 absolute z-10 bottom-0 left-0 w-full h-24 flex flex-col justify-center items-center">
-            <div className="z-20 absolute -top-5 w-full flex justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="group-hover:bg-white group-hover:text-orange-600 group-hover:rotate-180 w-10 h-10 bg-black text-white p-2 rounded-full transition-all"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
-                />
-              </svg>
-            </div>
-            <div className="group-hover:hidden transition-all duration-1000 w-4 absolute overflow-hidden inline-block right-0 -top-6">
-              <div className="h-6  bg-orange-900 -rotate-45 transform origin-bottom-right" />
-            </div>
-            <h2 className="font-bold">Bob haris</h2>
-            <span className="text-white">Sales executive</span>
-          </div>
-        </div>
-        <div className="group pb-24 relative overflow-hidden">
-          <div className="group-hover:translate-y-0 transition-all duration-700 translate-y-full top-0 right-0 bottom-24 left-0 absolute bg-gradient-to-b from-transparent to-orange-600 z-10" />
-          <img
-            src="https://picsum.photos/300/300"
-            className="absolute transition-all group-hover:scale-125 duration-700 mr-4 h-60 right-0"
-            alt=""
-          />
-          <div className="bg-orange-600 absolute z-10 bottom-0 left-0 w-full h-24 flex flex-col justify-center items-center">
-            <div className="z-20 absolute -top-5 w-full flex justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="group-hover:bg-white group-hover:text-orange-600 group-hover:rotate-180 w-10 h-10 bg-black text-white p-2 rounded-full transition-all"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
-                />
-              </svg>
-            </div>
-            <div className="group-hover:hidden transition-all duration-1000 w-4 absolute overflow-hidden inline-block right-0 -top-6">
-              <div className="h-6  bg-orange-900 -rotate-45 transform origin-bottom-right" />
-            </div>
-            <h2 className="font-bold">Bob haris</h2>
-            <span className="text-white">Sales executive</span>
-          </div>
+      <div className="max-w-screen mx-auto">
+        <div className="grid grid-cols-5 gap-8">
+          {bestSellers.map((book, i) => (
+            <BookCard key={i} data={book} isBestSeller={false}  />
+          ))}
         </div>
       </div>
     </div>
-  </div>
-  
-  )
+  );
 }
