e111034155e1460ed7fea66aa21ccfe8a3efbcda
ajummer <makeudreamempty@gmail.com>
2024-07-06 12:17:02 +0300

    fix: minor css bugs

--- client/readspace/src/app/catalog/page.jsx
+++ client/readspace/src/app/catalog/page.jsx
@@ -1,12 +1,12 @@
 import BookFilters from "../components/BookFilters/BookFilters.jsx";
 import BooksCatalog from "../components/BooksCatalog/BooksCatalog.jsx";
 
-BookFilters
+BookFilters;
 export default function Catalog() {
   return (
-    <div className="max-w-fit max-h-fit flex flex-col lg:flex-row ">  
-    <BookFilters/>
-    <BooksCatalog/>
+    <div className="max-w-fit min-h-screen flex flex-col lg:flex-row ">
+      <BookFilters />
+      <BooksCatalog />
     </div>
   );
 }
--- client/readspace/src/app/components/BooksCatalog/BooksCatalog.jsx
+++ client/readspace/src/app/components/BooksCatalog/BooksCatalog.jsx
@@ -2,7 +2,7 @@ import { bestSellers } from "../../../../utils/dummyData.js";
 import BookCard from "../BookCard/BookCard.jsx";
 export default function BooksCatalog() {
   return (
-    <div className="relative flex h-fit w-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+    <div className="relative flex h-full w-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
       <div className="max-w-screen mx-auto">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3 lg:grid-cols-4">
           {bestSellers.map((book, i) => (
