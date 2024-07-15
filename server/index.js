97ddbeccf173db1d92f01961498e41a077ded92c
ajummer <makeudreamempty@gmail.com>
2024-07-15 14:37:08 +0300

    feat: initliaze server & supabase

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
