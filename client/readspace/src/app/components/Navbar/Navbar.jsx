45ef3d5b5767dde6f1f4cba3a463a1f5505c48c3
ajummer <makeudreamempty@gmail.com>
2024-07-19 19:16:56 +0300

    feat: dynamically render navbar

--- client/readspace/src/app/components/Navbar/Navbar.jsx
+++ client/readspace/src/app/components/Navbar/Navbar.jsx
@@ -1,58 +1,43 @@
+"use client";
+import { useAuth } from "@/app/context/authContext.jsx";
+
 import Search from "../Search/Search.jsx";
 import Cart from "../Cart/Cart.jsx";
 import Profile from "../Profile/Profile.jsx";
+
 export default function Navbar() {
+  const { session } = useAuth();
+  console.log(session);
   return (
     <div className="navbar " data-theme="retro">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">ReadSpace</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <Search />
       </div>
+
       <div className="navbar-end">
-        <Cart />
-        <Profile />
+        <ul className="menu menu-lg menu-horizontal">
+          {session ? (
+            <>
+              <li>
+                <a>Catalog</a>
+              </li>
+              <Cart />
+              <Profile />
+            </>
+          ) : (
+            <>
+              <li>
+                <a href="/login">Login</a>
+              </li>
+              <li>
+                <a href="/register">Register</a>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
     </div>
   );
