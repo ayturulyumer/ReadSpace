1df0facad8ee4f588af132dbacc1b7b420d019cb
ajummer <makeudreamempty@gmail.com>
2024-08-25 15:32:05 +0300

    feat: add show hide password

--- client/readspace/src/app/settings/page.jsx
+++ client/readspace/src/app/settings/page.jsx
@@ -1,6 +1,22 @@
+"use client";
+import Image from "next/image.js";
+import ProfilePicture from "../../../public/samba.jpg";
+import { useAuth } from "../context/authContext.jsx";
+import { useState } from "react";
+
 export default function Settings() {
+  const { session } = useAuth();
+  const [passwordType, setPasswordType] = useState("password");
+  const handlePasswordType = () => {
+    if (passwordType === "password") {
+      setPasswordType("text");
+    } else {
+      setPasswordType("password");
+    }
+  };
+
   return (
-    <div className="py-24" data-theme="cupcake">
+    <div className="py-28 text-accent" data-theme="cupcake">
       <div className="mx-4 min-h-screen max-w-screen-xl sm:mx-8 xl:mx-auto">
         <h1 className="border-b py-6 text-4xl font-semibold">Settings</h1>
         <div className="grid grid-cols-8 pt-3 sm:grid-cols-10">
@@ -15,7 +31,7 @@ export default function Settings() {
               htmlFor="select-1"
               className="flex w-full cursor-pointer select-none rounded-lg border p-2 px-3 text-sm text-gray-700 ring-blue-700 peer-checked:ring"
             >
-              Accounts{" "}
+              Account{" "}
             </label>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -33,56 +49,86 @@ export default function Settings() {
             </svg>
             <ul className="max-h-0 select-none flex-col overflow-hidden rounded-b-lg shadow-md transition-all duration-300 peer-checked:max-h-56 peer-checked:py-3">
               <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-blue-700 hover:text-white">
-                Accounts
+                Account
               </li>
-              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-blue-700 hover:text-white">
-                Team
-              </li>
-              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-blue-700 hover:text-white">
-                Others
+              <li className=" cursor-pointer px-3 py-2 text-sm  text-slate-600 hover:bg-blue-700 hover:text-white">
+                Order History
+                <span className="indicator-item indicator-middle badge ml-1 font-medium badge-accent text-white">
+                  Coming Soon
+                </span>
               </li>
             </ul>
           </div>
           <div className="col-span-2 hidden sm:block">
             <ul>
-              <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
-                Teams
-              </li>
               <li className="mt-5 cursor-pointer border-l-2 border-l-blue-700 px-2 py-2 font-semibold text-blue-700 transition hover:border-l-blue-700 hover:text-blue-700">
-                Accounts
+                Account
               </li>
-              <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
-                Users
-              </li>
-              <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
-                Profile
-              </li>
-              <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
-                Billing
-              </li>
-              <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
-                Notifications
-              </li>
-              <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
-                Integrations
+              <li className="indicator mt-5 cursor-pointer border-l-2 border-transparent text-black px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
+                <span className="indicator-item indicator-center indicator-end badge ml-2   font-medium badge-accent text-white animate-pulse">
+                  Coming Soon
+                </span>
+                Order History
               </li>
             </ul>
           </div>
-          <div className="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
+          <div className="col-span-8 overflow-hidden py-4 rounded-xl border sm:bg-gray-50 sm:px-8 sm:shadow">
             <div className="pt-4">
               <h1 className="py-2 text-2xl font-semibold">Account settings</h1>
-              {/* <p class="font- text-slate-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> */}
             </div>
             <hr className="mt-4 mb-8" />
+            <div className="grid max-w-2xl  my-8">
+              <div className="flex flex-col items-center space-y-5 sm:flex-row sm:space-y-0">
+                <Image
+                  src={ProfilePicture}
+                  className="object-cover w-40 h-40 p-1 rounded-full ring-2 ring-indigo-300 dark:ring-orange-500"
+                />
+                <div className="flex flex-col space-y-5 sm:ml-8">
+                  <button
+                    type="button"
+                    className="py-3.5 px-7 text-base font-medium text-indigo-100 focus:outline-none bg-[#202142] rounded-lg border border-indigo-200 hover:bg-indigo-900 focus:z-10 focus:ring-4 focus:ring-indigo-200 "
+                  >
+                    Change picture
+                  </button>
+                  <button
+                    type="button"
+                    className="py-3.5 px-7 text-base font-medium text-indigo-900 focus:outline-none bg-white rounded-lg border border-indigo-200 hover:bg-indigo-100 hover:text-[#202142] focus:z-10 focus:ring-4 focus:ring-indigo-200 "
+                  >
+                    Delete picture
+                  </button>
+                </div>
+              </div>
+            </div>
             <p className="py-2 text-xl font-semibold">Email Address</p>
             <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-              <p className="text-gray-600">
-                Your email address is <strong>john.doe@company.com</strong>
-              </p>
-              <button className="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">
-                Change
-              </button>
+              <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
+                <label htmlFor="change-email">
+                  <span className="text-sm text-gray-500">Current Email</span>
+                  <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                    <input
+                      type="text"
+                      id="change-email"
+                      className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                      placeholder={session?.email}
+                    />
+                  </div>
+                </label>
+                <label htmlFor="change-email">
+                  <span className="text-sm text-gray-500">New Email</span>
+                  <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                    <input
+                      type="text"
+                      id="change-email"
+                      className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                      placeholder=""
+                    />
+                  </div>
+                </label>
+              </div>
             </div>
+            <button className=" mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white">
+              Save Email
+            </button>
             <hr className="mt-4 mb-8" />
             <p className="py-2 text-xl font-semibold">Password</p>
             <div className="flex items-center">
@@ -93,7 +139,7 @@ export default function Settings() {
                   </span>
                   <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
                     <input
-                      type="password"
+                      type={passwordType}
                       id="login-password"
                       className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
                       placeholder="***********"
@@ -104,7 +150,7 @@ export default function Settings() {
                   <span className="text-sm text-gray-500">New Password</span>
                   <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
                     <input
-                      type="password"
+                      type={passwordType}
                       id="login-password"
                       className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
                       placeholder="***********"
@@ -112,20 +158,24 @@ export default function Settings() {
                   </div>
                 </label>
               </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
+              <button
+                onClick={handlePasswordType}
                 className="mt-5 ml-2 h-6 w-6 cursor-pointer text-sm font-semibold text-gray-600 underline decoration-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                  />
+                </svg>
+              </button>
             </div>
             <p className="mt-2">
               Can't remember your current password.{" "}
@@ -139,33 +189,6 @@ export default function Settings() {
             <button className="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white">
               Save Password
             </button>
-            <hr className="mt-4 mb-8" />
-            <div className="mb-10">
-              <p className="py-2 text-xl font-semibold">Delete Account</p>
-              <p className="inline-flex items-center rounded-full bg-rose-100 px-4 py-1 text-rose-600">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="mr-2 h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                Proceed with caution
-              </p>
-              <p className="mt-2">
-                Make sure you have taken backup of your account in case you ever
-                need to get access to your data. We will completely wipe your
-                data. There is no way to access your account after this action.
-              </p>
-              <button className="ml-auto text-sm font-semibold text-rose-600 underline decoration-2">
-                Continue with deletion
-              </button>
-            </div>
           </div>
         </div>
       </div>
