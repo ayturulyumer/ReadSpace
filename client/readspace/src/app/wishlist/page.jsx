385f17c5d99270855dfc86655c6c32941e879deb
ajummer <makeudreamempty@gmail.com>
2024-09-01 18:15:21 +0300

    chore: fetch wishlist books

--- client/readspace/src/app/wishlist/page.jsx
+++ client/readspace/src/app/wishlist/page.jsx
@@ -1,20 +1,60 @@
 "use client";
 import BookCard from "../components/BookCard/BookCard.jsx";
 import EmptyState from "../components/EmptyState/EmptyState.jsx";
+import { useEffect, useState } from "react";
+import { getWishlist } from "../actions/wishlistActions.js";
+import { useAuth } from "../context/authContext.jsx";
+import Spinner from "../components/Spinner/Spinner.jsx";
+import toast from "react-hot-toast";
 
 export default function Wishlist() {
+  const { session } = useAuth();
+  const [wishlist, setWishlist] = useState();
+  const [loading, setIsLoading] = useState(true);
+
+  const userId = session?.id;
+
+  useEffect(() => {
+    if (userId) {
+      const fetchWishlist = async () => {
+        setIsLoading(true);
+        const { data, error } = await getWishlist(userId);
+        if (error) {
+          toast.error(error.message);
+          return;
+        }
+        setWishlist(data);
+        setIsLoading(false);
+      };
+      fetchWishlist();
+    }
+  }, [userId]);
+
+  console.log(wishlist);
+
+  if (loading) {
+    return <Spinner />; // Show spinner while loading
+  }
+
   return (
     <div className="text-secondary" data-theme="cupcake">
       <div className="mx-4 min-h-screen max-w-screen-xl sm:mx-8 xl:mx-auto">
-        <h1 className="border-b my-6 text-4xl font-semibold">Wishlist</h1>
-        <EmptyState />
-        <div className="grid py-4 grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {/* {[...Array(24)].map((_, index) => (
-            <div key={index} className="flex items-center justify-center">
-              <BookCard />
-            </div>
-          ))} */}
-        </div>
+        <h1 className="border-b my-6 text-4xl font-semibold">Your Wishlist</h1>
+
+        {wishlist.length === 0 ? (
+          <EmptyState />
+        ) : (
+          <div className="grid border py-4 grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {wishlist.map((item) => (
+              <div
+                key={item.book_id}
+                className="flex items-center justify-center"
+              >
+                <BookCard book={item} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
