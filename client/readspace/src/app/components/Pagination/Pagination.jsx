8ea857e0a1ef59e3f9a991944969781b1f1dfda5
ajummer <makeudreamempty@gmail.com>
2024-10-11 12:33:54 +0300

    chore: add more  reusable props to pagination

--- client/readspace/src/app/components/Pagination/Pagination.jsx
+++ client/readspace/src/app/components/Pagination/Pagination.jsx
@@ -1,14 +1,34 @@
 import { getPaginationRange } from "../../../../utils/getPaginationRange.js";
-export default function Pagination({ currentPage, totalPages, onPageChange }) {
+
+export default function Pagination({
+  currentPage,
+  totalPages,
+  onPageChange,
+  size = "md",
+}) {
   const paginationRange = getPaginationRange(currentPage, totalPages);
 
+  // Map the size prop to the appropriate Tailwind class
+  const sizeClassMap = {
+    xs: "btn-xs",
+    sm: "btn-sm",
+    md: "btn-md",
+    lg: "btn-lg",
+  };
+
+  // Determine the size class based on the provided size prop
+  const buttonSize = sizeClassMap[size] || sizeClassMap.md;
+
   return (
-    <div className="flex justify-center py-6 text-black">
+    <div className="flex justify-center  py-6 ">
       {/* Pagination container */}
-      <div className="join flex flex-wrap justify-center gap-2">
+      <div
+        data-theme="cupcake"
+        className="join flex flex-wrap justify-center gap-2"
+      >
         {/* Previous button */}
         <button
-          className="btn  join-item"
+          className={`btn join-item ${buttonSize}`}
           onClick={() => onPageChange(currentPage - 1)}
           disabled={currentPage === 1}
         >
@@ -19,7 +39,7 @@ export default function Pagination({ currentPage, totalPages, onPageChange }) {
         {paginationRange.map((page) => (
           <button
             key={page}
-            className={`btn join-item ${
+            className={`btn join-item ${buttonSize} ${
               currentPage === page ? "btn-accent" : ""
             }`}
             onClick={() => onPageChange(page)}
@@ -30,7 +50,7 @@ export default function Pagination({ currentPage, totalPages, onPageChange }) {
 
         {/* Next button */}
         <button
-          className="btn  join-item"
+          className={`btn join-item ${buttonSize}`}
           onClick={() => onPageChange(currentPage + 1)}
           disabled={currentPage === totalPages}
         >
