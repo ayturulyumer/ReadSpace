3777c7e2696cfe8506a234df63880e06d63051cf
ajummer <makeudreamempty@gmail.com>
2024-10-20 11:41:53 +0300

    chore: fix  layout issues in book details

--- client/readspace/src/app/components/SingleBookReview/SingleBookReview.jsx
+++ client/readspace/src/app/components/SingleBookReview/SingleBookReview.jsx
@@ -2,11 +2,11 @@ import Rating from "../Rating/Rating.jsx";
 
 export default function SingleBookReview({ review }) {
   return (
-    <div className="flex flex-col gap-3 my-4 rounded border  border-gray-300 text-xs md:text-medium">
+    <div className="flex flex-col gap-3 my-4 rounded border border-gray-300 text-xs md:text-medium">
       <div className="flex flex-col gap-4 p-4">
         {/* Profile and Rating */}
-        <div className="flex gap-4 justify-between ">
-          <div className="flex gap-4">
+        <div className="flex flex-col sm:flex-row gap-4 justify-between sm:items-center">
+          <div className="flex gap-4 items-center">
             <div className="avatar">
               <div className="w-8 h-8 rounded-full">
                 <img src={review?.user_avatar} alt="userAvatar" />
@@ -14,11 +14,14 @@ export default function SingleBookReview({ review }) {
             </div>
             <span>{review?.username}</span>
           </div>
-          <Rating
-            rating={review?.rating}
-            name={review?.review_text}
-            isDisabled={true}
-          />
+          {/* Move rating below on small screens, and beside username on larger screens */}
+          <div className="sm:ml-4">
+            <Rating
+              rating={review?.rating}
+              name={review?.review_text}
+              isDisabled={true}
+            />
+          </div>
         </div>
         <div className="font-medium">
           {review?.review_text}
