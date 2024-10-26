f95d039b40e7b5b68f7a3cb8ffcd5f9455167f2a
ajummer <makeudreamempty@gmail.com>
2024-10-26 11:29:54 +0300

    chore: persist redux state in localstorage

---
+++ client/readspace/src/app/lib/storage.js
@@ -0,0 +1,22 @@
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+const createNoopStorage = () => {
+  return {
+    getItem() {
+      return Promise.resolve(null);
+    },
+    setItem(value) {
+      return Promise.resolve(value);
+    },
+    removeItem() {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createNoopStorage();
+
+export default storage;
--- client/readspace/src/app/lib/store.js
+++ client/readspace/src/app/lib/store.js
@@ -1,10 +1,53 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "./storage.js";
 import cartReducer from "../components/Cart/cartSlice.js";
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      cart: cartReducer,
-    },
-  });
+// Root reducer combining reducers if there are multiple
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  // other reducers can be added here
+});
+
+// Persist config
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"], // only persist the cart slice
 };
+// Persisted Reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Used to create a new store for each render
+// export const makeStore = () => {
+//   return configureStore({
+//     reducer: persistedReducer,
+//     middleware: (getDefaultMiddleware) =>
+//       getDefaultMiddleware({
+//         serializableCheck: {
+//           ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//         },
+//       }),
+//   });
+// };
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
